df01b84f03822cd283994d082d1d34eb971e7c50
mohanasundaramt <tmohanasundaram.55@gmail.com>
2024-09-01 15:32:17 +0530

    assignment react

--- src/Components/contactmodel.tsx
+++ src/Components/contactmodel.tsx
@@ -1,19 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Dialog,
   DialogActions,
   DialogContent,
   DialogTitle,
-  IconButton,
   Button,
+  TextField,
+  MenuItem,
+  Typography,
 } from "@mui/material";
 import ContactModalProps from "../interfaces/contactmodel";
 
+// Color options for the group color selector
+const colorOptions = [
+  { label: "Blue", value: "#0047FF" },
+  { label: "Purple", value: "#B38BFA" },
+  { label: "Cyan", value: "#43E6FC" },
+  { label: "Pink", value: "#FF66F0" },
+  { label: "Orange", value: "#F19576" },
+  { label: "Red", value: "#FF0000" },
+];
 
-const ContactModel: React.FC<ContactModalProps> = ({
-  open,
-  onClose,
-}) => {
+const ContactModel: React.FC<ContactModalProps> = ({ open, onClose }) => {
+  const [groupName, setGroupName] = useState<string>("");
+  const [groupColor, setGroupColor] = useState<string>("");
+
+  const handleGroupNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setGroupName(event.target.value);
+  };
+
+  const handleGroupColorChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setGroupColor(event.target.value);
+  };
+
+  const handleCreateGroup = () => {
+    // Handle group creation logic here
+    console.log("Group Name:", groupName);
+    console.log("Group Color:", groupColor);
+    onClose(); // Close the dialog after creating the group
+  };
 
   return (
     <Dialog
@@ -22,30 +47,67 @@ const ContactModel: React.FC<ContactModalProps> = ({
       sx={{
         "& .MuiDialog-paper": {
           backgroundColor: "white",
-          width: "100%",
+         // width: "400px",
           boxShadow: "0px 4px 4px 0px #00000040",
-          border: "1px solid #0000001F",
-          padding: "10px 30px 10px 10px",
-          margin: "0px 10px 500px 100px",
+        //  border: "1px solid #0000001F",
+         // padding: "20px",
+          //margin: "0px 10px 500px 100px",
         },
       }}
     >
-         <DialogTitle
-         sx={{
-            fontFamily: "Roboto",
-            fontSize: "29px",
-            fontWeight: 500,
-            lineHeight: "46.45px",
-            letterSpacing: "0.035em",
-            textAlign: "left",
-            display: "flex",
-            color: "#000000",
-          }}
-          
-       >
-          Create New group
-        </DialogTitle>
-        
+      <DialogTitle
+        sx={{
+          fontFamily: "Roboto",
+          fontSize: "24px",
+          fontWeight: 500,
+          lineHeight: "32px",
+          textAlign: "left",
+          color: "#000000",
+          paddingBottom: "10px",
+        }}
+      >
+        Create New Group
+      </DialogTitle>
+      <DialogContent>
+        <Typography variant="body1" sx={{ marginBottom: "8px" }}>
+          Group Name
+        </Typography>
+        <TextField
+          fullWidth
+          placeholder="Enter group name"
+          value={groupName}
+          onChange={handleGroupNameChange}
+          variant="outlined"
+          sx={{ marginBottom: "20px" }}
+        />
+
+        <Typography variant="body1" sx={{ marginBottom: "8px" }}>
+          Group Color
+        </Typography>
+        <TextField
+          select
+          fullWidth
+          value={groupColor}
+          onChange={handleGroupColorChange}
+          variant="outlined"
+          sx={{ marginBottom: "20px" }}
+        >
+          {colorOptions.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
+      </DialogContent>
+      <DialogActions sx={{ justifyContent: "flex-end" }}>
+        <Button
+          onClick={handleCreateGroup}
+          color="primary"
+          disabled={!groupName || !groupColor}
+        >
+          Create
+        </Button>
+      </DialogActions>
     </Dialog>
   );
 };
